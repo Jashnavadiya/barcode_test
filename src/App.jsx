3e7a37dca75ac6a9f81ffd5063a033b71386e7ee
Jashnavadiya <jashnavadiya7@gmail.com>
2024-10-23 13:48:27 +0530

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import BarcodeGenerator from './BarcodeGenerator';
 import BarcodeScanner from './BarcodeScanner';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { HashRouter as Router,Routes,Route } from 'react-router-dom';
 
 const App = () => {
     return (
